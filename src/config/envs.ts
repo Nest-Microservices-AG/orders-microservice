e7ddc55e4c60e2b3b41d29270824acbdf9652371
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-06-08 20:39:36 -0400

    first commit orders microservice

---
+++ src/config/envs.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvironmentVars {
+  PORT: number;
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config valudation error: ${error.message}`);
+}
+
+const environmentsVariables: EnvironmentVars = value;
+
+export const envs = {
+  port: environmentsVariables.PORT,
+};
